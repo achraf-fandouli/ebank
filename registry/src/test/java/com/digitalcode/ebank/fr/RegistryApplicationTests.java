af7b7e837dadd1331462102fa0c5a1b56832277d
achraf-fandouli <achraf.fandouli@gmail.com>
2023-12-22 23:16:40 +0100

    create all projects

---
+++ registry/src/test/java/com/digitalcode/ebank/fr/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcode.ebank.fr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

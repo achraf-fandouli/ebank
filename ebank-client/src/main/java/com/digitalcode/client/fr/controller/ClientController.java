7104af94f557f3622568af42b3e5667d142acb3a
achraf-fandouli <achref.fendouli@gmail.com>
2023-12-27 23:43:18 +0100

    CRUD client

--- ebank-client/src/main/java/com/digitalcode/client/fr/controller/ClientController.java
+++ ebank-client/src/main/java/com/digitalcode/client/fr/controller/ClientController.java
@@ -1,11 +1,20 @@
 package com.digitalcode.client.fr.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.digitalcode.client.fr.entity.dto.ClientDto;
@@ -32,7 +41,35 @@ public class ClientController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<ClientDto> getClient(@PathVariable Long id) throws Exception {
-		ClientDto response = MapperUtil.convertClientToDto(clientService.getClienttById(id));
+		ClientDto response = MapperUtil.convertClientToDto(clientService.getClientById(id));
 		return ResponseEntity.ok().body(response);
 	}
+
+	@PutMapping
+	public ResponseEntity<ClientDto> updateClient(@RequestBody ClientDto clientDto) throws Exception {
+		Client client = MapperUtil.convertDtoTOClient(clientDto);
+		ClientDto response = MapperUtil.convertClientToDto(clientService.updateClient(client));
+		return ResponseEntity.ok().body(response);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity deleteClienttById(@PathVariable Long id) throws Exception {
+		try {
+			clientService.deleteClienttById(id);
+			return new ResponseEntity<>("Successfully deleted the client with Id: " + id, HttpStatus.CREATED);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>("No client is found !!", HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<ClientDto>> findAllClient() {
+		List<Client> clients = clientService.findAllClient();
+		List<ClientDto> response = new ArrayList();
+		for (Client client : clients) {
+			response.add(MapperUtil.convertClientToDto(client));
+		}
+		return ResponseEntity.ok().body(response);
+	}
+
 }
--- ebank-client/src/main/java/com/digitalcode/client/fr/service/ClientService.java
+++ ebank-client/src/main/java/com/digitalcode/client/fr/service/ClientService.java
@@ -1,7 +1,9 @@
 package com.digitalcode.client.fr.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.digitalcode.client.fr.entity.jpa.Client;
@@ -18,16 +20,38 @@ public class ClientService {
 
 	public Client saveClient(Client client) {
 		return clientRepository.save(client);
-
 	}
 
-	public Client getClienttById(Long id) throws Exception {
+	public Client updateClient(Client clientInput) throws Exception {
+		Optional<Client> result = clientRepository.findById(clientInput.getId());
+		if (result.isPresent()) {
+			BeanUtils.copyProperties(clientInput, result.get());
+			return clientRepository.save(result.get());
+		} else {
+			throw new Exception("Client Not Exist with Id: "+ clientInput.getId());
+		}
+	}
+
+	public Client getClientById(Long id) throws Exception {
 		Optional<Client> client = clientRepository.findById(id);
 		if (client.isPresent()) {
 			return client.get();
 		} else {
-			throw new Exception("Client Not Fond");
+			throw new Exception("Client Not Fond with Id: "+ id);
 		}
 	}
-
+	
+	public void deleteClienttById(Long id) throws Exception{
+		Optional<Client> client = clientRepository.findById(id);
+		if (client.isPresent()) {
+			clientRepository.deleteById(id);
+		} else {
+			throw new Exception("Client Not Exist with Id: "+ id);
+		}
+	}
+	
+	public List<Client> findAllClient(){
+		return clientRepository.findAll();
+	}
 }
+
